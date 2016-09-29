5e5c01e4537a8232baaf1ab296915a06c1294d5b
Duarte David <deltaduartedavid@gmail.com>
2016-09-29 21:43:22 +0200

    Add simple scalar parser implementation

--- src/yaml.rs
+++ src/yaml.rs
@@ -66,16 +66,44 @@ fn parse_f64(v: &str) -> Option<f64> {
     }
 }
 
-pub struct YamlLoader {
+/// A `YamlScalarParser` is a parser that change the parsing of a yaml scalar value
+/// like a tag
+///
+/// # Examples
+///
+/// ```
+///use yaml_rust::yaml;
+///use yaml_rust::scanner;
+///
+///struct HelloTagParser;
+///
+///impl yaml::YamlScalarParser for HelloTagParser {
+///    fn parse_scalar(&self, tag: &scanner::TokenType, value: &str) -> Option<yaml::Yaml> {
+///        if let scanner::TokenType::Tag(ref handle, ref suffix) = *tag {
+///            if *handle == "!" && *suffix == "hello" {
+///                return Some(yaml::Yaml::String("Hello ".to_string() + value))
+///            }
+///        }
+///        None
+///    }
+///}
+/// ```
+pub trait YamlScalarParser {
+    fn parse_scalar(&self , tag: &TokenType , value: &str) -> Option<Yaml>;
+}
+
+#[derive(Default)]
+pub struct YamlLoader<'a> {
     docs: Vec<Yaml>,
     // states
     // (current node, anchor_id) tuple
     doc_stack: Vec<(Yaml, usize)>,
     key_stack: Vec<Yaml>,
     anchor_map: BTreeMap<usize, Yaml>,
+    scalar_parser: Vec<&'a YamlScalarParser>
 }
 
-impl MarkedEventReceiver for YamlLoader {
+impl<'a> MarkedEventReceiver for YamlLoader<'a> {
     fn on_event(&mut self, ev: Event, _: Marker) {
         // println!("EV {:?}", ev);
         match ev {
@@ -107,6 +135,17 @@ impl MarkedEventReceiver for YamlLoader {
                 self.insert_new_node(node);
             },
             Event::Scalar(v, style, aid, tag) => {
+                if let Some(ref tag) = tag {
+                    let mut yaml = None;
+                    for parser in &self.scalar_parser {
+                        yaml = parser.parse_scalar(tag , &v);
+                    }
+                    if let Some(yaml) = yaml {
+                        self.insert_new_node((yaml, aid));
+                        return;
+                    }
+                }
+
                 let node = if style != TScalarStyle::Plain {
                     Yaml::String(v)
                 } else if let Some(TokenType::Tag(ref handle, ref suffix)) = tag {
@@ -163,7 +202,7 @@ impl MarkedEventReceiver for YamlLoader {
     }
 }
 
-impl YamlLoader {
+impl<'a> YamlLoader<'a> {
     fn insert_new_node(&mut self, node: (Yaml, usize)) {
         // valid anchor id starts from 1
         if node.1 > 0 {
@@ -192,16 +231,28 @@ impl YamlLoader {
         }
     }
 
+    pub fn register_scalar_parser(&mut self , parser: &'a YamlScalarParser){
+        self.scalar_parser.push(parser);
+    }
+
     pub fn load_from_str(source: &str) -> Result<Vec<Yaml>, ScanError>{
-        let mut loader = YamlLoader {
+        YamlLoader::new().parse_from_str(source)
+    }
+
+    pub fn new() -> YamlLoader<'a> {
+        YamlLoader {
             docs: Vec::new(),
             doc_stack: Vec::new(),
             key_stack: Vec::new(),
             anchor_map: BTreeMap::new(),
-        };
+            scalar_parser: Vec::new()
+        }
+    }
+
+    pub fn parse_from_str(mut self , source: &str) -> Result<Vec<Yaml>, ScanError>{
         let mut parser = Parser::new(source.chars());
-        try!(parser.load(&mut loader, true));
-        Ok(loader.docs)
+        try!(parser.load(&mut self, true));
+        Ok(self.docs)
     }
 }
 
@@ -375,6 +426,7 @@ impl Iterator for YamlIter {
 #[cfg(test)]
 mod test {
     use yaml::*;
+    use scanner::*;
     use std::f64;
     #[test]
     fn test_coerce() {
@@ -637,5 +689,28 @@ c: ~
         let out = YamlLoader::load_from_str(&s).unwrap();
         let first = out.into_iter().next().unwrap();
         assert_eq!(first[0]["important"].as_bool().unwrap(), true);
+    }
+
+    struct HelloTagParser;
+
+    impl YamlScalarParser for HelloTagParser {
+        fn parse_scalar(&self, tag: &TokenType, value: &str) -> Option<Yaml> {
+            if let TokenType::Tag(ref handle, ref suffix) = *tag {
+                if *handle == "!" && *suffix == "hello" {
+                    return Some(Yaml::String("Hello ".to_string() + value))
+                }
+            }
+            None
+        }
+    }
+
+    #[test]
+    fn test_scalar_parser() {
+        let parser = HelloTagParser;
+        let mut loader = YamlLoader::new();
+        loader.register_scalar_parser(&parser);
+        let out = loader.parse_from_str("- !hello world").unwrap();
+        let doc = &out[0];
+        assert_eq!(doc[0].as_str().unwrap() , "Hello world")
     }
 }
