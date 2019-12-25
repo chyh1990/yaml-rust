040439287ecc7bb586b4649101858e1728dafde6
Tom Parker-Shemilt <palfrey@tevp.net>
2019-12-25 18:21:10 +0000

    Fix erroring clippy lints about if let options

--- src/yaml.rs
+++ src/yaml.rs
@@ -289,14 +289,14 @@ impl Yaml {
     pub fn from_str(v: &str) -> Yaml {
         if v.starts_with("0x") {
             let n = i64::from_str_radix(&v[2..], 16);
-            if n.is_ok() {
-                return Yaml::Integer(n.unwrap());
+            if let Ok(integer) = n {
+                return Yaml::Integer(integer);
             }
         }
         if v.starts_with("0o") {
             let n = i64::from_str_radix(&v[2..], 8);
-            if n.is_ok() {
-                return Yaml::Integer(n.unwrap());
+            if let Ok(integer) = n {
+                return Yaml::Integer(integer);
             }
         }
         if v.starts_with('+') && v[1..].parse::<i64>().is_ok() {
